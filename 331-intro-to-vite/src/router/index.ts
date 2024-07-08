93db5b454a6c3c53f1ad5d3da4c7b67de209f177
DeformiaR <https://github.com/DeformiaR>
2024-07-08 21:51:02 +0700

    11.1

--- 331-intro-to-vite/src/router/index.ts
+++ 331-intro-to-vite/src/router/index.ts
@@ -1,13 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import EventListView from '../views/EventsListView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Event-List-view',
+      component: EventListView
     },
     {
       path: '/about',
