a5fabbbcfed453b3fc1f0530e9b2c9e45996f43b
DeformiaR <https://github.com/DeformiaR>
2024-07-08 15:51:47 +0700

    7

--- 331-intro-to-vite/src/views/HomeView.vue
+++ 331-intro-to-vite/src/views/HomeView.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+</script>
 
+<div class="home">
+  <EventCard />
+</div>
 
 <template>
   <div class="Home"></div>
 </template>
+
+
